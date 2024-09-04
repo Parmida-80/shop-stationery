a07247a8e7a4f6b032f13df70b482f988ebe1e3b
Parmida <parmidaamiryarahmady@gmail.com>
2024-09-04 17:10:40 +0330

    Part2

--- asset/js/inexJs.js
+++ asset/js/inexJs.js
@@ -1,27 +1,9 @@
-document.getElementById('toggleButton').addEventListener('click', function() {
-    var list = document.getElementById('list');
-    if (list.style.visibility === 'hidden') {
-        list.style.visibility = 'visible';
+document.getElementById('menu').addEventListener('click', function () {
+    var leaderList = document.getElementById('leader');
+    if (leaderList.style.visibility === 'hidden') {
+        leaderList.style.visibility = 'visible';
     } else {
-        list.style.visibility = 'hidden';
+        leaderList.style.visibility = 'hidden';
     }
 });
-
-var navbarItem = document.querySelector('.navbar:nth-child(5)');
-
-// دریافت لیست کشویی
-var dropdown = navbarItem.querySelector('.dropdown');
-
-// اضافه کردن رویداد برای نمایش لیست کشویی هنگام قرارگیری موس روی آیتم
-navbarItem.addEventListener('mouseover', function() {
-    dropdown.style.display = 'block';
-});
-
-// اضافه کردن رویداد برای مخفی کردن لیست کشویی هنگام برداشتن موس از روی آیتم
-navbarItem.addEventListener('mouseout', function() {
-    dropdown.style.display = 'none';
-});
-
-
-
 
